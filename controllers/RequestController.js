7aee6257f015d3e9da0a75097eb786611252cbaa
לנג לאה < @mbjcomp.local>
2024-06-18 14:09:01 +0300

    updates

--- controllers/RequestController.js
+++ controllers/RequestController.js
@@ -4,7 +4,7 @@ import service from '../services/RequestService.js';
 class RequestController extends BaseController{
     constructor(service){
         super(service);
-        this.type = "request";
+        this.type = "help request";
     }
 
     async update(req, res, next) {
